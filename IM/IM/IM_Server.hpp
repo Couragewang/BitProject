793e520aa9f1d75be03fd9a91e95819ee25f6d82
whb <295851252@qq.com>
2019-06-06 03:06:14 -0700

    add mysql in

--- IM/IM/IM_Server.hpp
+++ IM/IM/IM_Server.hpp
@@ -5,19 +5,110 @@
 #include <string>
 #include <signal.h>
 #include "mongoose.h"
+#include "mysql.h"
 
 #define WEB_ROOT "web"
+#define OFFLINE "offline"
+#define ONLINE "online"
 
+#define HOST       "localhost"
+#define USER       "root"
+#define PASSWD     ""
+#define DB         "IM_2019_6_5"
+#define PORT       3306
+#define USER_TB    "user"
+#define MESSAGE_TB "message"
+
+static std::string InfoToString(std::string &str)
+{
+    std::string tmp = "\'";
+    tmp += str;
+    tmp += "\'";
+    return tmp;
+}
+
+//model
+class IM_MysqlClient{
+    private:
+        MYSQL *conn;
+    public:
+        IM_MysqlClient()
+        {
+            conn = mysql_init(NULL);
+            mysql_real_connect(conn, HOST, USER, PASSWD, DB, PORT, NULL, 0);
+        }
+        bool InsertUser(std::string &_name, std::string &_passwd, std::string &_status)
+        {
+            std::string sql = "INSERT INTO ";
+            sql += USER_TB;
+            sql += "(name, passwd, status) values (";
+            sql += InfoToString(_name);
+            sql += ",";
+            sql += InfoToString(_passwd);
+            sql += ",";
+            sql += InfoToString(_status);
+            sql += ")";
+            int result = mysql_query(conn, sql.c_str());
+            return result == 0 ? true : false;
+            //for debug
+            //std::cout << "result:" << result <<" sql:" << sql << std::endl;
+        }
+        bool SelectUser(std::string &_name, std::string &_passwd)
+        {
+            return true;
+        }
+        void UpdateUserStatus();
+        void InsertMessage();
+        void SelectMessage();
+        ~IM_MysqlClient()
+        {
+            mysql_close(conn);
+        }
+};
+
+class IM_Controller{
+    private:
+        IM_MysqlClient client;
+    public:
+        IM_Controller()
+        {
+        }
+        ~IM_Controller()
+        {
+        }
+        bool AddUser(std::string _name,std::string _passwd, std::string _status)
+        {
+            return client.InsertUser(_name, _passwd, _status);
+        }
+        bool IsUserLegal(std::string _name, std::string _passwd)
+        {
+            return client.SelectUser(_name, _passwd);
+        }
+        void SetUserStatus();
+        void AddMessage();
+        void SelectMessage();
+};
+
+static std::string mr_str_to_string(struct mg_str *str)
+{
+    std::string outstring;
+    for(auto i = 0; i < str->len; i++){
+        outstring.push_back(str->p[i]);
+    }
+    return outstring;
+}
+//http server选项
+struct mg_serve_http_opts http_opts;
 class IM_Server{
     private:
         std::string port;
         static volatile bool quit;
-        //http server选项
-        struct mg_serve_http_opts http_opts;
         //mongoose 事件管理器
         struct mg_mgr mgr;
         //listen socket 对应的connect
         struct mg_connection *nc;
+        //访问用户数据库
+        static IM_Controller ctr;
     public:
         IM_Server(std::string _port = "8080")
             :port(_port)
@@ -32,22 +123,76 @@ class IM_Server{
         }
         static void ProcessRegister(struct mg_connection *nc, struct http_message *hm)
         {
-            struct http_message *hm = (struct http_message*)ev_data;
-            std::cout << (char*)hm->body.p << std::endl;
+            //for test
+            char name[64];
+            char passwd[128];
+            memset(name,0, sizeof(name));
+            memset(passwd,0, sizeof(passwd));
+            mg_get_http_var(&hm->body, "name", name, sizeof(name));
+            mg_get_http_var(&hm->body, "passwd", passwd, sizeof(passwd));
+
+            std::string _name = name;
+            std::string _passwd = passwd;
+            std::string _status = OFFLINE;
+            //for test
+            _name = "none";
+            _passwd = "none";
+            if(ctr.AddUser(_name, _passwd, _status)){
+
+            }
+        }
+        static bool ProcessLogin(struct mg_connection *nc, struct http_message *hm)
+        {
+            //for test
+            std::string name = "zhangsan";
+            std::string passwd="12345";
+            return ctr.IsUserLegal(name, passwd);
+        }
+        static void Broadcast(struct mg_connection *nc, std::string info)
+        {
+            struct mg_connection *c;
+            for(c = mg_next(nc->mgr, NULL); c != NULL; c = mg_next(nc->mgr, c)){
+                //if(nc == c) continue;
+                mg_send_websocket_frame(c, WEBSOCKET_OP_TEXT, info.c_str(), info.size());
+            }
+        }
+        static bool IsWebsocket(const struct mg_connection *nc)
+        {
+            return (nc->flags & MG_F_IS_WEBSOCKET) ? true : false;
+        }
+        static void HandlerResponse(struct mg_connection *nc, struct http_message *hm)
+        {
+            //std::cout << (char*)hm->body.p <<std::endl;
         }
         static void EventHandler(struct mg_connection *nc, int ev, void *ev_data)
         {
             switch(ev){
-                case MG_EV_HTTP_REQUEST:
-                    {
+                case MG_EV_HTTP_REQUEST:{
+                        struct http_message *hm = (struct http_message*)ev_data;
                         if(mg_vcmp(&hm->uri, "/register") == 0){
-                            ProcessRegister();
+                            ProcessRegister(nc, hm);
+                            //HandlerResponse(nc, hm);
                         }
-                        else if(mg_vcmp(&hm->uri, "/login") == 0){
-                            //ProcessLogin();
+                        else if(mg_vcmp(&hm->uri, "/login") == 0 && ProcessLogin(nc, hm)){
+                            //std::cout << "uri:"<< hm->uri.p <<" hello login" << std::endl;
                         }
                         else{
-                            std::cout << "unknow" << std::endl;
+                            mg_serve_http(nc, hm, http_opts);
+                        }
+                    }
+                    break;
+                case MG_EV_WEBSOCKET_HANDSHAKE_DONE:
+                    Broadcast(nc, "any body ++ join");
+                    break;
+                case MG_EV_WEBSOCKET_FRAME:{
+                        struct websocket_message *wm = (struct websocket_message*)ev_data;
+                        std::string message = (char*)wm->data;
+                        Broadcast(nc, message);
+                    }
+                    break;
+                case MG_EV_CLOSE:{
+                        if(IsWebsocket(nc)){
+                            Broadcast(nc, "any body ++ left");
                         }
                     }
                     break;
@@ -73,6 +218,5 @@ class IM_Server{
         }
 };
 volatile bool IM_Server::quit = false;
+IM_Controller IM_Server::ctr;
 #endif
-
-
--- IM/IM/main.cc
+++ IM/IM/main.cc
@@ -4,18 +4,32 @@ using namespace std;
 
 static void Usage(std::string proc)
 {
-    std::cout << "Usage: " << proc << " port [default:8080]" << std::endl;
+    std::cout << "Usage: " << proc << " port" << std::endl;
+    std::cout << "Notice: "<<"\n\tDefault Port: 8080" << "\n\tPort Range: [1024-9999]"<< std::endl;
 }
-
+static bool IsPortOk(const char* _port, std::string &port_out)
+{
+    int p = atoi(_port);
+    if(p >= 1024 && p <= 9999){
+        port_out = _port;
+        return true;
+    }
+    return false;
+}
 int main(int argc, char *argv[])
 {
-    if(argc != 2){
-        Usage(argv[0]);
-        return 1;
+    int ret = 0;
+    std::string port = "8080";
+    if( (argc == 2 && IsPortOk(argv[1], port)) || argc == 1 ){
+        IM_Server isp(port);
+        isp.InitServer();
+        isp.Run();
     }
-    IM_Server isp(argv[1]);
-    isp.InitServer();
-    isp.Run();
+    else{
+        Usage(argv[0]);
+        ret = 1;
+    }
+    return ret;
 }
 
 
